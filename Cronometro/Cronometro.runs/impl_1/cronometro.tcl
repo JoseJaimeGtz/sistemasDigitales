ad6ef94bb8a213557103b745ec14a74758c60218
JoseJaimeGtz <42259245+JoseJaimeGtz@users.noreply.github.com>
2021-04-28 22:41:00 -0500

    uartTX

--- Cronometro/Cronometro.runs/impl_1/cronometro.tcl
+++ Cronometro/Cronometro.runs/impl_1/cronometro.tcl
@@ -61,100 +61,13 @@ proc step_failed { step } {
 }
 
 
-start_step init_design
-set ACTIVE_STEP init_design
-set rc [catch {
-  create_msg_db init_design.pb
-  set_param chipscope.maxJobs 4
-  set_param xicom.use_bs_reader 1
-  create_project -in_memory -part xc7a35tcpg236-1
-  set_property design_mode GateLvl [current_fileset]
-  set_param project.singleFileAddWarning.threshold 0
-  set_property webtalk.parent_dir C:/Users/Jonathan/Downloads/sistemasDigitales-main/Cronometro/Cronometro.cache/wt [current_project]
-  set_property parent.project_path C:/Users/Jonathan/Downloads/sistemasDigitales-main/Cronometro/Cronometro.xpr [current_project]
-  set_property ip_output_repo C:/Users/Jonathan/Downloads/sistemasDigitales-main/Cronometro/Cronometro.cache/ip [current_project]
-  set_property ip_cache_permissions {read write} [current_project]
-  add_files -quiet C:/Users/Jonathan/Downloads/sistemasDigitales-main/Cronometro/Cronometro.runs/synth_1/cronometro.dcp
-  read_xdc C:/Users/Jonathan/Downloads/sistemasDigitales-main/Cronometro/Cronometro.srcs/constrs_1/new/Basys3.xdc
-  link_design -top cronometro -part xc7a35tcpg236-1
-  close_msg_db -file init_design.pb
-} RESULT]
-if {$rc} {
-  step_failed init_design
-  return -code error $RESULT
-} else {
-  end_step init_design
-  unset ACTIVE_STEP 
-}
-
-start_step opt_design
-set ACTIVE_STEP opt_design
-set rc [catch {
-  create_msg_db opt_design.pb
-  opt_design 
-  write_checkpoint -force cronometro_opt.dcp
-  create_report "impl_1_opt_report_drc_0" "report_drc -file cronometro_drc_opted.rpt -pb cronometro_drc_opted.pb -rpx cronometro_drc_opted.rpx"
-  close_msg_db -file opt_design.pb
-} RESULT]
-if {$rc} {
-  step_failed opt_design
-  return -code error $RESULT
-} else {
-  end_step opt_design
-  unset ACTIVE_STEP 
-}
-
-start_step place_design
-set ACTIVE_STEP place_design
-set rc [catch {
-  create_msg_db place_design.pb
-  if { [llength [get_debug_cores -quiet] ] > 0 }  { 
-    implement_debug_core 
-  } 
-  place_design 
-  write_checkpoint -force cronometro_placed.dcp
-  create_report "impl_1_place_report_io_0" "report_io -file cronometro_io_placed.rpt"
-  create_report "impl_1_place_report_utilization_0" "report_utilization -file cronometro_utilization_placed.rpt -pb cronometro_utilization_placed.pb"
-  create_report "impl_1_place_report_control_sets_0" "report_control_sets -verbose -file cronometro_control_sets_placed.rpt"
-  close_msg_db -file place_design.pb
-} RESULT]
-if {$rc} {
-  step_failed place_design
-  return -code error $RESULT
-} else {
-  end_step place_design
-  unset ACTIVE_STEP 
-}
-
-start_step route_design
-set ACTIVE_STEP route_design
-set rc [catch {
-  create_msg_db route_design.pb
-  route_design 
-  write_checkpoint -force cronometro_routed.dcp
-  create_report "impl_1_route_report_drc_0" "report_drc -file cronometro_drc_routed.rpt -pb cronometro_drc_routed.pb -rpx cronometro_drc_routed.rpx"
-  create_report "impl_1_route_report_methodology_0" "report_methodology -file cronometro_methodology_drc_routed.rpt -pb cronometro_methodology_drc_routed.pb -rpx cronometro_methodology_drc_routed.rpx"
-  create_report "impl_1_route_report_power_0" "report_power -file cronometro_power_routed.rpt -pb cronometro_power_summary_routed.pb -rpx cronometro_power_routed.rpx"
-  create_report "impl_1_route_report_route_status_0" "report_route_status -file cronometro_route_status.rpt -pb cronometro_route_status.pb"
-  create_report "impl_1_route_report_timing_summary_0" "report_timing_summary -max_paths 10 -file cronometro_timing_summary_routed.rpt -pb cronometro_timing_summary_routed.pb -rpx cronometro_timing_summary_routed.rpx -warn_on_violation "
-  create_report "impl_1_route_report_incremental_reuse_0" "report_incremental_reuse -file cronometro_incremental_reuse_routed.rpt"
-  create_report "impl_1_route_report_clock_utilization_0" "report_clock_utilization -file cronometro_clock_utilization_routed.rpt"
-  create_report "impl_1_route_report_bus_skew_0" "report_bus_skew -warn_on_violation -file cronometro_bus_skew_routed.rpt -pb cronometro_bus_skew_routed.pb -rpx cronometro_bus_skew_routed.rpx"
-  close_msg_db -file route_design.pb
-} RESULT]
-if {$rc} {
-  write_checkpoint -force cronometro_routed_error.dcp
-  step_failed route_design
-  return -code error $RESULT
-} else {
-  end_step route_design
-  unset ACTIVE_STEP 
-}
-
 start_step write_bitstream
 set ACTIVE_STEP write_bitstream
 set rc [catch {
   create_msg_db write_bitstream.pb
+  set_param chipscope.maxJobs 1
+  open_checkpoint cronometro_routed.dcp
+  set_property webtalk.parent_dir C:/sistemasDigitales/Cronometro/Cronometro.cache/wt [current_project]
   catch { write_mem_info -force cronometro.mmi }
   write_bitstream -force cronometro.bit 
   catch {write_debug_probes -quiet -force cronometro}
